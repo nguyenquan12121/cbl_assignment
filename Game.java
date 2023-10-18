59e5023c1c9ada4b59d2c74a02175030d85fc037
quan <quan177013@gmail.com>
2023-10-18 21:10:13 +0200

    Add template files for game logic + Added round, player and game file

---
+++ Game.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+class Game{
+    private Integer id;
+    Random random;
+    final int ROUND_COUNT = 3;
+    private int currRound = 1;
+    private Integer currScore;
+    private Player player;
+    public Game(){
+        random = new Random();
+        id = random.nextInt(100000, 999999);
+    }
+    public void updateRound(){
+        currRound++;
+    }
+    public int getRound(){
+        return currRound;
+    }
+    public Player getPlayer(String userName){
+        player = new Player(currScore, userName);
+        return player;
+    }
+    public Integer getID(){
+        return id;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,13 @@
+class Player{
+    Integer score;
+    String username;
+    /**
+     * @param score
+     * @param username
+     */
+    public Player(Integer score, String username) {
+        this.score = score;
+        this.username = username;
+    }
+    
+}
