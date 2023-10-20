f021f8bff2840a005358d26f5d987316caf70aa3
quan <quan177013@gmail.com>
2023-10-20 10:23:04 +0200

    Update menu screen + Added some styling to welcome text + Added Leaderboards button

--- MenuContainer.java
+++ MenuContainer.java
@@ -1,10 +1,15 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 
 
@@ -14,20 +19,30 @@ public class MenuContainer {
     }
     public static void addComponentsToPane (JFrame frame)  {
         JPanel buttonPanel=new JPanel();
+        buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        buttonPanel.setLayout(new GridBagLayout());
         //create panel to hold 2 buttons
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.PAGE_AXIS));
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
         //add button to the pane
+        JLabel jl = new JLabel("<html><h1><strong><i>Hot Highstriker</i></strong></h1><hr></html>");
         JButton play = new JButton("Play!");
+        JButton leaderboard = new JButton("Show leaderboard!");
         JButton exit = new JButton("Exit!");
-        buttonPanel.add(play);
-        buttonPanel.add(exit);
+        buttonPanel.add(jl, gbc);
+        buttonPanel.add(play, gbc);
+        buttonPanel.add(leaderboard, gbc);
+        buttonPanel.add(exit, gbc);
+        gbc.weighty = 1;
         buttonPanel.setBackground(Color.red);
         play.addActionListener(e ->{
             frame.dispose();
             new Game();
         });
         exit.addActionListener(e -> System.exit(-1));
-        frame.add(buttonPanel);
+        frame.add(buttonPanel, BorderLayout.CENTER);
 
     }
 
@@ -36,6 +51,7 @@ public class MenuContainer {
         //Create and set up the window.
         JFrame frame = new JFrame("Welcome Screen");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setPreferredSize(new Dimension(1280, 720));
         //Set up content pane
         addComponentsToPane(frame);
